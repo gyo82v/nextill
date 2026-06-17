715130d47606dfe0f1e4eac5841864d498abdd02
gyo82v <gyo82v@gmail.com>
2026-06-17 18:15:18 +0100

    feat: update the stock page

--- components/stock/StockList.tsx
+++ components/stock/StockList.tsx
@@ -2,7 +2,7 @@
 
 import { useMemo, useState } from "react";
 import StockItemCard from "./StockItemCard";
-import type { StockListProps } from "@/types";
+import type { StockListProps, StockCategory } from "@/types";
 import Select from "../ui/select";
 import { inputBaseStyle } from "@/styles";
 import { DotLineDivider } from "@/components/ui/dividers/Dividers";
@@ -11,15 +11,23 @@ import { useTranslation } from "react-i18next";
 
 export default function StockList({ uid, items }: StockListProps) {
   const [query, setQuery] = useState("");
-  const [category, setCategory] = useState<"all" | "food" | "drink">("all");
+  const [category, setCategory] = useState<"all" | "food" | "drink" | "packaging" | "disposableItems" | "cleaningSupplies" | "other">("all");
   const {t} = useTranslation("stock")
 
   const categoryLabel =
     category === "food"
     ? t("stockSection.food")
     : category === "drink"
-      ? t("stockSection.drink")
-      : t("stockSection.all");
+    ? t("stockSection.drink")
+    : category === "packaging"
+    ? t("stockSection.packaging")
+    : category === "disposableItems"
+    ? t("stockSection.disposableItems")
+    : category === "cleaningSupplies"
+    ? t("stockSection.cleaningSupplies")
+    : category === "other"
+    ? t("stockSection.other")
+    : undefined
 
   const filteredItems = useMemo(() => {
     return items.filter((item) => {
@@ -45,12 +53,16 @@ export default function StockList({ uid, items }: StockListProps) {
           className={`${inputBaseStyle} flex-2`}
         />
 
-        <Select.Root value={category} onValueChange={setCategory} className="flex-1">
+        <Select.Root value={category} onValueChange={setCategory} className="flex-2">
           <Select.Trigger label={categoryLabel} />
           <Select.Content>
             <Select.Item value="all">{t("stockSection.all")}</Select.Item>
             <Select.Item value="food">{t("stockSection.food")}</Select.Item>
             <Select.Item value="drink">{t("stockSection.drink")}</Select.Item>
+            <Select.Item value="packaging">{t("stockSection.packaging")}</Select.Item>
+            <Select.Item value="disposableItems">{t("stockSection.disposableItems")}</Select.Item>
+            <Select.Item value="cleaningSupplies">{t("stockSection.cleaningSupplies")}</Select.Item>
+            <Select.Item value="other">{t("stockSection.other")}</Select.Item>
           </Select.Content>
         </Select.Root>
       </div>
